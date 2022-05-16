fe2e5a0487928dd6e598885258361d2e60cc675e
erinCodes3276 <72887582+erinCodes3276@users.noreply.github.com>
2022-05-16 15:15:38 -0400

    Preliminary commenting for functionality

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,17 @@
 from prompts import Prompts
 from model import Model
 
-# Return a Prompts object
+# Return a Prompts objects
+# prompt = """ """"
+#line to read the JSON file
+# create object to load the file json.loads(prompt) 
+#.loads() needs to load a string
+
+# example of how to parse a JSON file.
+#with open('path_to_file/person.json', 'r') as f:
+#  data = json.load(f)
+#print(data)
+
 def load_prompts(prompt_config, pattern):
     pass
 
