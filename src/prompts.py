6389bd46d469ddef574dc096d86d68dbb2feb251
Erin Atacan <erin3276@gmail.com>
2022-06-07 14:14:15 -0400

    prompts.py initialization & method implementation

--- src/prompts.py
+++ src/prompts.py
@@ -2,5 +2,16 @@
 
 class Promps(object):
     def __init__(self):
-	self.object = object
-
+	self.examples = []
+	self.inputs = []
+	self.output = []
+	self.pattern = []
+    def validate():
+	for key_and_val in self.examples:
+		if key in key_and_val and value == key_and_val[key]:
+			#I want to say while this is true, pass to the next key
+			pass
+    def complete(prompt_config_entry):
+	for k in prompt_config_entry:
+		prompt = prompt.replace('{'+k+'}',prompt_config[0][k])
+	
