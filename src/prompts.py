ffe4afba3564433de82c029029407f829d5b98b2
Erin Atacan <erin3276@gmail.com>
2022-06-14 11:38:10 -0400

    added prompts class functionality

--- src/prompts.py
+++ src/prompts.py
@@ -2,16 +2,24 @@
 
 class Promps(object):
     def __init__(self):
-	self.examples = []
-	self.inputs = []
-	self.output = []
-	self.pattern = []
+        self.examples = []
+        self.inputs = []
+        self.output = []
+        self.pattern = []
+
     def validate():
-	for key_and_val in self.examples:
-		if key in key_and_val and value == key_and_val[key]:
-			#I want to say while this is true, pass to the next key
-			pass
+        for key_and_val in self.examples:
+             while key in key_and_val and value == key_and_val[key]:
+                 True
+
     def complete(prompt_config_entry):
-	for k in prompt_config_entry:
-		prompt = prompt.replace('{'+k+'}',prompt_config[0][k])
-	
+        for k in prompt_config_entry:
+            prompt = prompt.replace('{'+k+'}',prompt_config[0][k])
+
+    def compile_inputs():
+        for i in self.examples:
+            self.inputs.append(i.keys())
+
+    def compile outputs():
+        for i in self.examples:
+            self.output.append(i.values())
