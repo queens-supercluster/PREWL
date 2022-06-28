b95d61279cce9bc468dc305e9fc2960dfbee1fc6
Erin Atacan <erin3276@gmail.com>
2022-06-28 15:55:38 -0400

    gpt2.py

--- prewl/endpoints/gpt2.py
+++ prewl/endpoints/gpt2.py
@@ -1,9 +1,10 @@
-from transformers import pipeline, set_seed
+from prewl.endpoints.base import Endpoint
 
-generator = pipeline('text-generation', model='gpt2')
-
-set_seed(42)
-
-text_generator = generator("Some Text", max_length=30, num_return_sequences=1)
-
-print(text_generator)
+class GPT2(Endpoint):
+    
+    def call(self, prompt):
+        from transformers import pipeline, set_seed
+        generator = pipeline('text-generation', model='gpt2')
+        set_seed(42)
+        text_generator = generator(prompt, max_length=30, num_return_sequences=1)
+        return text_generator
