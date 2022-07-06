0deb351d43e701a088491234e8d680b3b0ad9695
Erin Atacan <erin3276@gmail.com>
2022-07-06 14:52:01 -0400

    text generation gpt2

--- prewl/endpoints/gpt2.py
+++ prewl/endpoints/gpt2.py
@@ -3,8 +3,16 @@ from prewl.endpoints.base import Endpoint
 class GPT2(Endpoint):
     
     def call(self, prompt):
-        from transformers import pipeline, set_seed
-        generator = pipeline('text-generation', model='gpt2')
-        set_seed(42)
-        text_generator = generator(prompt, max_length=30, num_return_sequences=1)
-        return text_generator
+        import tensorflow as tf
+        from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+
+        # encode context the generation is conditioned on
+        input_ids = tokenizer.encode(prompt, return_tensors='tf')
+
+        # generate text until the output length (which includes the context length) reaches 50
+        greedy_output = model.generate(input_ids, max_length=100)
+
+        return tokenizer.decode(greedy_output[0], skip_special_tokens=True)
+
