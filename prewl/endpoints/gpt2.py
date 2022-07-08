f94f1fb91dc8e9646587983614ffd7b5aec8a32c
Erin Atacan <erin3276@gmail.com>
2022-07-08 13:09:48 -0400

    edit comments in gpt2.py

--- prewl/endpoints/gpt2.py
+++ prewl/endpoints/gpt2.py
@@ -3,19 +3,23 @@ from prewl.endpoints.base import Endpoint
 class GPT2(Endpoint):
     
     def call(self, prompt):
+        # Avoids warnings.
         import os
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
         import tensorflow as tf
         tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
         from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
         tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        
+        # Add the EOS token as PAD token to avoid warnings.
         model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
 
-        # encode context the generation is conditioned on
+        # Encode context the text generation is conditioned to.
         input_ids = tokenizer.encode(prompt, return_tensors='tf')
 
-        # generate text until the output length (which includes the context length) reaches 50
+        # Generates text until the output length using greedy search as the decoding method.
         greedy_output = model.generate(input_ids, max_length=100)
 
-        return tokenizer.decode(greedy_output[0], skip_special_tokens=True)
+        print(tokenizer.decode(greedy_output[0], skip_special_tokens=True))
 
