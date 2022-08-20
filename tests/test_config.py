cc939582fa74048590e44e676d5d2be4d4e7dc77
Christian Muise <christian.muise@gmail.com>
2022-08-20 01:07:48 -0300

    Hybrid test.

--- tests/test_config.py
+++ tests/test_config.py
@@ -20,3 +20,21 @@ class TestConfig:
             prewl.configure(tmp.name)
             assert 'test' in prewl.CONFIG
             assert prewl.CONFIG['test'] == 123
+    
+    def test_hybrid(self):
+        prewl.reset_config()
+        assert 'test1' not in prewl.CONFIG
+        assert 'test2' not in prewl.CONFIG
+        assert 'test3' not in prewl.CONFIG
+        prewl.configure({'test1': 123, 'test2': 123})
+        assert prewl.CONFIG['test1'] == 123
+        assert prewl.CONFIG['test2'] == 123
+
+        with tempfile.NamedTemporaryFile() as tmp:
+            with open(tmp.name, 'w') as f:
+                f.write('{"test2": 321, "test3": 321}')
+            prewl.configure(tmp.name)
+            assert prewl.CONFIG['test1'] == 123
+            assert prewl.CONFIG['test2'] == 321
+            assert prewl.CONFIG['test3'] == 321
+            
